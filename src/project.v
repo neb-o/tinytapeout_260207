66b79916b837a1b60b096809a109a06a39ea248a
Bastian Neussell <bastian.neussell@outlook.com>
2026-02-08 13:05:11 +0100

    test commit

--- src/project.v
+++ src/project.v
@@ -6,7 +6,7 @@
 `default_nettype none
 
 module tt_um_example (
-    input  wire [7:0] ui_in,    // Dedicated inputs
+    input  wire [7:0] ui_in,    // Dedicated inputs lalala
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
     output wire [7:0] uio_out,  // IOs: Output path
